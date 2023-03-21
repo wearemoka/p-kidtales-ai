1661ece73dac8d3ec90d30ac994846e5eb2cb6b3
Akash Patel <akash@moka.tv>
2023-03-21 19:06:59 +0530

    Generate Random Stories

--- src/app/service/constants/StoryParams.tsx
+++ src/app/service/constants/StoryParams.tsx
@@ -1,4 +1,4 @@
 export const ages = ['2-5', '5-7', '7-10']
-export const characters = ['dog', 'cat', 'goose']
-export const adventures = ['adventure', 'fable', 'love']
-export const places = ['mountain', 'big city', 'farm']
+export const characters = ['Dog', 'Cat', 'Goose', 'Tiger', 'Elephant', 'Rabbit', 'Tortoise', 'Lion', 'Fox', 'Sheep', 'Rat', 'Ant', 'Grasshopper', 'Cow', 'Goat', 'Bear', 'Fish']
+export const adventures = ['Adventure', 'Fable', 'Love', 'Funny', 'Fairy', 'Folklore', 'Animal Tale']
+export const places = ['Mountain', 'Big City', 'Farm', 'Ocean', 'River', 'Zoo', 'Museum', 'Jungle', 'Village', 'Circus', 'Stadium', 'Pond']
