2bf558236d00d6520e13bbed11c60ba2798b1f64
Akash Patel <akash@moka.tv>
2023-03-30 15:43:03 +0530

    List and Display previuosly generated stories

--- src/app/story/list/page.tsx
+++ src/app/story/list/page.tsx
@@ -1,21 +1,20 @@
 'use client'
 import { useFetchStory } from '@/app/hooks/useFetchStory'
 import { updateDocumentInFireStore } from '@/app/service/FirebaseService'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import Style from './list.module.css'
 
 const List = () => {
   const fireBaseStoryCollection = process.env.NEXT_PUBLIC_FIREBASE_STORE_STORY_END_POINT as string
   const { data, setData } = useFetchStory(fireBaseStoryCollection)
-  const router = useRouter()
+  /**
+   * Edit a Story
+   * Route to edit page
+   */
 
-  const viewStoryHandler = (id: string) => {
-    router.push(`/story/view/${id}`)
-  }
-
-  const editStoryHandler = (id: string) => {
-    router.push(`/story/edit/${id}`)
-  }
+  // const editStoryHandler = (id: string) => {
+  //   router.push(`/story/edit/${id}`)
+  // }
 
   /**
    * Flag a Story for inappropriate content or language
@@ -36,7 +35,6 @@ const List = () => {
           <tr>
             <th className={Style.tableColumnCell}>No</th>
             <th className={Style.tableColumnCell}>Title</th>
-            <th className={Style.tableColumnCell}>Description</th>
             <th className={Style.tableColumnCell}>Is Appropriate?</th>
             <th className={Style.tableColumnCell}>Actions</th>
           </tr>
@@ -48,11 +46,9 @@ const List = () => {
                 <tr key={`${index}`}>
                   <td className={Style.tableRowCell}>{index + 1}</td>
                   <td className={Style.tableRowCell}>{item.title}</td>
-                  <td className={Style.tableRowCell}>{item.description}</td>
                   <td className={Style.tableRowCell}>{(item.appropriate == null || item.appropriate) ? '✅' : '❌'}</td>
                   <td className={Style.tableRowCell}>
-                    <button className={Style.actionEditButton} onClick={() => editStoryHandler(item.id)}>Edit</button>
-                    <button className={Style.actionViewButton} onClick={() => viewStoryHandler(item.id)}>View</button>
+                    <Link className={Style.actionViewButton} href={`/story/view/${item.id}`}>View</Link>
                     <button onClick={() => banStory(item)} disabled={!(item.appropriate == null || item.appropriate)}>Inappropriate</button>
                   </td>
                 </tr>
