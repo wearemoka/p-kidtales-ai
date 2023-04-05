338d4bfd0a19e14ffcffe15cb00b400d2e398329
Ivan Schweikofski <schweikofski@gmail.com>
2023-04-05 08:33:08 -0300

    Change default view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 import { Inter } from 'next/font/google'
-import AddStory from './story/add/page'
+import Story from './story/page'
 // eslint-disable-next-line no-unused-vars
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
-    <AddStory />
+    <Story />
   )
 }
