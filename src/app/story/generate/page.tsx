83e0de7e7a33292e0ce81b0d7807d784121b71c1
Ivan Schweikofski <schweikofski@gmail.com>
2023-04-10 12:25:45 -0300

    Remove Share content and dependencies

--- src/app/story/generate/page.tsx
+++ src/app/story/generate/page.tsx
@@ -1,13 +1,7 @@
 import Illustration from '@/app/components/generators/Illustration'
 import PreSelectedHistory from '@/app/components/generators/PreSelectedHistory'
-import ShareContent from '@/app/components/generators/ShareContent'
 import RandomStory from '../random/RandomStory'
 import styles from './generate.module.css'
-
-// Moked data to share as example
-const urlToShare: string = 'http://localhost:3000/demo'
-const quote: string = 'This is an AI-generated story'
-const hashtags: string[] = ['#iaStory', '#bedStory', '#IA']
 
 /**
  * This is a general page to show the different integrations with AI,
@@ -26,13 +20,7 @@ function StoryFormPage () {
       <hr />
 
       <Illustration />
-      <hr />
 
-      <ShareContent
-        urlToShare={urlToShare}
-        quote={quote}
-        hashtags={hashtags}
-      />
     </main>
   )
 }
