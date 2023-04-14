7937fbbfac0d163a3f67853c7a6ee37d9e07ecad
Ivan Schweikofski <schweikofski@gmail.com>
2023-04-14 10:27:42 -0300

    Added voices to choice

--- src/app/components/Speakers/LovoReader.tsx
+++ src/app/components/Speakers/LovoReader.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 interface Props {
   text: string;
@@ -7,11 +7,27 @@ interface Props {
 
 const LovoReader = ({ text }: Props) => {
   const audioRef = useRef<HTMLAudioElement>(null)
+  const [skinVoices, setSkinVoices] = useState()
+  const [providers, setProviders] = useState('Jemima Taylor')
+
+  useEffect(() => {
+    fetch('/api/speakers/lovo/voices')
+      .then((res) => res.json())
+      .then((data) => {
+        const voices = data.res.data
+          .filter((v:any) => {
+            return v.tags.some((vv:any) => {
+              return (vv.content === 'British' || vv.content === 'American')
+            })
+          })
+        setSkinVoices(voices)
+      })
+  }, [])
 
   const handleClick = async () => {
     const bodyRequest = {
       text,
-      speaker_id: 'Martha Sage'
+      speaker_id: providers
     }
 
     const res = await fetch('/api/speakers/lovo', {
@@ -34,6 +50,17 @@ const LovoReader = ({ text }: Props) => {
 
   return (
     <>
+      {
+      skinVoices &&
+        <select
+          value={providers}
+          onChange={(e) => {
+            setProviders(e.target.value)
+          }}
+        >
+          {skinVoices.map((sv:any) => <option key={sv.name} value={sv.name}>{sv.name}</option>)}
+        </select>
+      }
       <button onClick={handleClick}>Load Lovo AI</button>
       <audio
         id='reader'
