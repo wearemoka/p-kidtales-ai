1cf76503b4ca27a3a94115582f41d2832e0632c9
Ivan Schweikofski <schweikofski@gmail.com>
2023-04-24 07:36:29 -0300

    show/hide buttons on topbar

--- src/app/components/TopBar/TopBar.tsx
+++ src/app/components/TopBar/TopBar.tsx
@@ -4,15 +4,26 @@ import styles from './TopBar.module.scss'
 import { usePathname, useRouter } from 'next/navigation'
 import { Button, Stack, Image, Container } from '@chakra-ui/react'
 import BackButton from './BackButton'
+import { useGlobalContext } from '@/app/context/store'
 
 const TopBar = () => {
   const router = useRouter()
   const pathname = usePathname()
   const [showBackButton, setShowBackButton] = useState(false)
+  const [showFlagButton, setShowFlagButton] = useState(false)
+  const { BGMusic, setBGMusic } = useGlobalContext()
 
   useEffect(() => {
     setShowBackButton(pathname !== '/')
+    setShowFlagButton(pathname === '/story/generate')
   }, [pathname])
+
+  /**
+   * Changes the global status of background music
+   */
+  const musicOnOffButtonClick = () => {
+    setBGMusic(!BGMusic)
+  }
 
   return (
     <div className={styles.topbar}>
@@ -25,12 +36,26 @@ const TopBar = () => {
           </div>
 
           <Stack direction='row' spacing={{ base: 1, md: 4 }} className={styles.actions}>
-            <Button aria-label='Go to Library' rightIcon={<Image src='icons/Library.svg' alt='Books outline white icon' />}>
+            <Button
+              aria-label='Go to Library'
+              rightIcon={<Image src='icons/Library.svg' alt='Books outline white icon' />}
+              onClick={() => {
+                router.push('/story/list')
+              }}
+            >
               <label>Library</label>
             </Button>
 
-            <Button aria-label='Music on/off' rightIcon={<Image src='icons/Music.svg' alt='Books outline white icon' />}>
-              <label>Music On</label>
+            <Button
+              aria-label='Music on/off'
+              rightIcon={<Image src='icons/Music.svg' alt='Books outline white icon' />}
+              onClick={musicOnOffButtonClick}
+            >
+              {
+              BGMusic
+                ? <label>Music Off</label>
+                : <label>Music On</label>
+              }
             </Button>
 
             <Button
@@ -43,15 +68,16 @@ const TopBar = () => {
               <label>Info</label>
             </Button>
 
-            <Button
-              aria-label='Flag tale as inappropriate'
-              rightIcon={<Image src='icons/Flag.svg' alt='Books outline white icon' />}
-              onClick={() => {
-                router.push('/story/list')
-              }}
-            >
-              <label>Flag tale</label>
-            </Button>
+            {showFlagButton &&
+              <Button
+                aria-label='Flag tale as inappropriate'
+                rightIcon={<Image src='icons/Flag.svg' alt='Books outline white icon' />}
+                onClick={() => {
+                  console.log('Flag this story.')
+                }}
+              >
+                <label>Flag tale</label>
+              </Button>}
           </Stack>
         </div>
       </Container>
