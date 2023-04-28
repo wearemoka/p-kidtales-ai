dc1c9cc25934fae31550eaa617772f872945eda0
Ivan Schweikofski <schweikofski@gmail.com>
2023-04-28 12:14:44 -0300

    Edited constants, added image

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -4,48 +4,62 @@ import NameSelect from '@/app/components/NameSelect/NameSelect'
 import { useGlobalContext } from '@/app/context/store'
 import UserPrompt from '@/app/components/UserPrompt/UserPrompt'
 import { characterOpts, lessonOpts, PROMPT_STEPS, scenarioOpts } from '@/app/utils/constants'
-import { Button, Center, VStack } from '@chakra-ui/react'
+import { Button, Center, Image, VStack } from '@chakra-ui/react'
 import { getAiStory } from '@/app/services/ChatGPTService'
+import { useState } from 'react'
+import { useMessageTime } from '@/app/hooks/useMessageTime'
 
 const StoryPage = () => {
   const { globalPrompt } = useGlobalContext()
+  const [isLoadingStory, setIsLoadingStory] = useState<boolean>(false)
+  const loadingMessages = useMessageTime(isLoadingStory)
 
   const writeStoryHandler = async () => {
+    setIsLoadingStory(true)
     const { age, character, name, scenario, lesson } = globalPrompt
     const response = await getAiStory(age, character, name, scenario, lesson)
     console.log(response)
+    setIsLoadingStory(false)
   }
 
   return (
     <VStack>
-
       <Center>Create a story for {globalPrompt.age}</Center>
 
       {/* Display the User prompt */}
       <UserPrompt promptOptions={globalPrompt} steps={PROMPT_STEPS} />
 
-      <Center>
-        {/* Display Character options */}
-        {globalPrompt.step === PROMPT_STEPS.CHARACTER && <GallerySelect title='Select a character' options={characterOpts} saveOn='character' />}
+      {!isLoadingStory && (
+        <Center>
+          {/* Display Character options */}
+          {globalPrompt.step === PROMPT_STEPS.CHARACTER && <GallerySelect title='Select a character' options={characterOpts} saveOn='character' />}
 
-        {/* Display Name input */}
-        {globalPrompt.step === PROMPT_STEPS.NAME && <NameSelect title='Name your character' saveOn='name' />}
+          {/* Display Name input */}
+          {globalPrompt.step === PROMPT_STEPS.NAME && <NameSelect title='Name your character' saveOn='name' />}
 
-        {/* Display Scensario options */}
-        {globalPrompt.step === PROMPT_STEPS.SCENARIO && <GallerySelect title='Select a scenario' options={scenarioOpts} saveOn='scenario' />}
+          {/* Display Scensario options */}
+          {globalPrompt.step === PROMPT_STEPS.SCENARIO && <GallerySelect title='Select a scenario' options={scenarioOpts} saveOn='scenario' />}
 
-        {/* Display Lesson options */}
-        {globalPrompt.step === PROMPT_STEPS.LESSON &&
-          <VStack>
-            <GallerySelect title='Select a lesson' options={lessonOpts} saveOn='lesson' />
-            <Button
-              variant='outline'
-              onClick={writeStoryHandler}
-            >
-              Write my Own Lesson
-            </Button>
-          </VStack>}
-      </Center>
+          {/* Display Lesson options */}
+          {globalPrompt.step === PROMPT_STEPS.LESSON &&
+            <VStack>
+              <GallerySelect title='Select a lesson' options={lessonOpts} saveOn='lesson' />
+              <Button
+                variant='outline'
+                onClick={writeStoryHandler}
+              >
+                Write my Own Lesson
+              </Button>
+            </VStack>}
+        </Center>
+      )}
+
+      {isLoadingStory && (
+        <VStack>
+          <Image src='images/Initial.png' alt='Wizard magic loading' />
+          <h3>{loadingMessages}</h3>
+        </VStack>
+      )}
     </VStack>
   )
 }
--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -3,9 +3,9 @@
  * These messages will be displayed when making requests to the server
  */
 export const LoadingMessages = [
-  'Loading',
-  'This is taking longer than expected',
-  'Please wait a little more time'
+  'Adding some magic...',
+  'Training young wizards...',
+  'Creating your character...'
 ]
 
 export const characterOpts = [
