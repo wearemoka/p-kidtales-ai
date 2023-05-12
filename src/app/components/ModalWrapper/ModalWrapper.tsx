2fff7ee65c18da814884fd7bc65f644bd14107fe
florlapetina <florencia@moka.tv>
2023-05-12 12:26:15 -0300

    About modal

--- src/app/components/ModalWrapper/ModalWrapper.tsx
+++ src/app/components/ModalWrapper/ModalWrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button } from '@chakra-ui/react'
+import { Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Container, Grid, GridItem } from '@chakra-ui/react'
 import React, { ReactNode } from 'react'
 
 interface Props {
@@ -17,33 +17,37 @@ interface Props {
 function ModalWrapper ({ isOpen, onClose, modalTitle, children, primaryActionLabel, primaryAction, secondaryActionLabel, secondaryAction }: Props) {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{modalTitle}</ModalHeader>
         <ModalCloseButton />
-
-        <ModalBody>
-          {children}
-        </ModalBody>
-
-        <ModalFooter>
-          {primaryActionLabel &&
-            <Button
-              colorScheme='blue'
-              mr={3}
-              onClick={primaryAction}
-            >
-              {primaryActionLabel}
-            </Button>}
-          {secondaryActionLabel &&
-            <Button
-              colorScheme='cyan'
-              onClick={secondaryAction}
-            >
-              {secondaryActionLabel}
-            </Button>}
-        </ModalFooter>
-
+        <Container>
+          <Grid templateColumns='repeat(12, 1fr)' gap={4}>
+            <GridItem colSpan={{ lg: 2, md: 0, base: 0 }} />
+            <GridItem colSpan={{ lg: 8, md: 12, base: 12 }}>
+              <ModalHeader className='heading-small'>{modalTitle}</ModalHeader>
+              <ModalBody className='body'>
+                {children}
+              </ModalBody>
+              <ModalFooter>
+                {primaryActionLabel &&
+                  <Button
+                    colorScheme='blue'
+                    mr={3}
+                    onClick={primaryAction}
+                  >
+                    {primaryActionLabel}
+                  </Button>}
+                {secondaryActionLabel &&
+                  <Button
+                    colorScheme='cyan'
+                    onClick={secondaryAction}
+                  >
+                    {secondaryActionLabel}
+                  </Button>}
+              </ModalFooter>
+            </GridItem>
+            <GridItem colSpan={{ lg: 2, md: 0, base: 0 }} />
+          </Grid>
+        </Container>
       </ModalContent>
     </Modal>
   )
