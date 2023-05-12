87c184f1e9fe0e8d92bfa394577686abc8302e45
florlapetina <florencia@moka.tv>
2023-05-12 07:39:43 -0300

    remove story pagination

--- src/app/story/view/page.tsx
+++ src/app/story/view/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 /* eslint-disable react-hooks/rules-of-hooks */
-import { StoryPagination } from '@/app/components/StoryPagination/StoryPagination'
 import { useGlobalContext } from '@/app/context/store'
-import { Heading, Container, GridItem, Grid, Image } from '@chakra-ui/react'
+import { Heading, Container, GridItem, Grid, Image, Button, Text } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import styles from './tale.module.scss'
+import Steps from '@/app/components/Steps/Steps'
 
 function viewPage () {
   const { globalStory, setGlobalStory } = useGlobalContext()
   const [title, setTitle] = useState<string>('')
+  const pages = globalStory.storyPaged.length
 
   useEffect(() => {
     const tmpStory = { ...globalStory }
@@ -23,21 +24,44 @@ function viewPage () {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  const setCurrentStoryPage = (page: number) => {
+    const tmpStory = { ...globalStory }
+    tmpStory.currentPage = page
+    setGlobalStory(tmpStory)
+  }
+
+  const handleNextPage = () => {
+    if (globalStory.currentPage < pages - 1) {
+      setCurrentStoryPage(globalStory.currentPage + 1)
+    }
+  }
+
+  const handlePrevPage = () => {
+    if (globalStory.currentPage > 1) {
+      setCurrentStoryPage(globalStory.currentPage - 1)
+    }
+  }
+
   return (
     <div className={styles.tale}>
-      <div className={styles.image}>
-        <Image src='images/characters/whale.png' alt='' />
-      </div>
+      <Image src='images/characters/whale.png' alt='' />
       <Container>
         <Grid templateColumns='repeat(12, 1fr)' gap={4}>
           <GridItem colSpan={{ lg: 3, md: 1, base: 0 }} />
           <GridItem colSpan={{ lg: 6, md: 10, base: 12 }}>
 
             <Heading as='h1' className='heading-small' mb={3}>{title}</Heading>
-            <StoryPagination />
+            <Text className='lead'>
+              {globalStory.storyPaged[globalStory.currentPage]}
+            </Text>
+            <div className={styles.buttonsWrapper}>
+              <Button className='big secondary only-icon' rightIcon={<Image src='/icons/Arrow-Left.svg' alt='Arrow right outline white icon' />} disabled={globalStory.currentPage <= 1} onClick={handlePrevPage} />
+              <Button className='big secondary only-icon' rightIcon={<Image src='/icons/Arrow-Right.svg' alt='Arrow right outline white icon' />} disabled={globalStory.currentPage >= pages - 1} onClick={handleNextPage} />
+            </div>
+
+            <Steps currentStep={globalStory.currentPage} size={pages} />
 
           </GridItem>
-          <GridItem colSpan={{ lg: 3, md: 1, base: 0 }} />
         </Grid>
       </Container>
     </div>
