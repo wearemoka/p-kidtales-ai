a941b2b3804ddf4f5216335c1b95c1432bc2ffd2
Ivan Schweikofski <schweikofski@gmail.com>
2023-05-12 13:19:54 -0300

    Added Slick-slider to story

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout ({
       <head>
         <meta name='viewport' content='width=device-width,initial-scale=1' />
         <link rel='manifest' href='/manifest.json' />
+        <link rel='stylesheet' type='text/css' charSet='UTF-8' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
+        <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css' />
       </head>
 
       <body>
--- src/app/story/view/page.tsx
+++ src/app/story/view/page.tsx
@@ -1,84 +1,66 @@
 'use client'
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useGlobalContext } from '@/app/context/store'
-import { Heading, Container, GridItem, Grid, Image, Button, Text, useDisclosure } from '@chakra-ui/react'
+import { Heading, Container, Image, Text, useDisclosure } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import styles from './tale.module.scss'
-import Steps from '@/app/components/Steps/Steps'
 import ModalOverlayWrapper from '@/app/components/ModalWrapper/ModalOverlayWrapper'
 import { useRouter } from 'next/navigation'
+import { ROUTES } from '@/app/utils/routes'
+import Slider from 'react-slick'
 
 function viewPage () {
-  const { globalStory, setGlobalStory } = useGlobalContext()
+  const { globalStory } = useGlobalContext()
   const [title, setTitle] = useState<string>('')
-  const pages = globalStory.storyPaged.length
   const { isOpen, onOpen, onClose } = useDisclosure()
   const router = useRouter()
 
   useEffect(() => {
-    const tmpStory = { ...globalStory }
-    tmpStory.currentPage = 1
-    setGlobalStory(tmpStory)
-
-    const story = globalStory.storyPaged || null
-    if (story && story.length > 0) {
-      const cleanTitle = story[0].replace('Title: ', '')
+    const tmpStory = globalStory.storyPaged || null
+    if (tmpStory && tmpStory.length > 0) {
+      const cleanTitle = tmpStory[0].replace('Title: ', '')
       setTitle(cleanTitle)
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const setCurrentStoryPage = (page: number) => {
-    const tmpStory = { ...globalStory }
-    tmpStory.currentPage = page
-    setGlobalStory(tmpStory)
-  }
-
-  const handleNextPage = () => {
-    if (globalStory.currentPage < pages - 1) {
-      setCurrentStoryPage(globalStory.currentPage + 1)
-    } else { // no more pages
-      onOpen()
-    }
-  }
-
-  const handlePrevPage = () => {
-    if (globalStory.currentPage > 1) {
-      setCurrentStoryPage(globalStory.currentPage - 1)
-    }
-  }
-
   const openLibrary = () => {
-    router.replace('/library')
+    router.replace(ROUTES.LIBRARY)
   }
 
   const generateNewStory = () => {
-    router.replace('/')
+    router.replace(ROUTES.HOME)
+  }
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    afterChange: function (index) {
+      console.log(
+        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+      )
+    }
   }
 
   return (
     <div className={styles.tale}>
       <Image src='images/characters/whale.png' alt='' />
       <Container>
-        <Grid templateColumns='repeat(12, 1fr)' gap={4} className={styles.taleWrapper}>
+        <Heading as='h1' className='heading-small' mb={3} mt={10}>{title}</Heading>
 
-          <GridItem colSpan={{ lg: 2, md: 1, base: 0 }} className={styles.buttonWrapper}>
-            <Button className='big secondary only-icon' rightIcon={<Image src='/icons/Arrow-Left.svg' alt='Arrow right outline white icon' />} disabled={globalStory.currentPage <= 1} onClick={handlePrevPage} />
-          </GridItem>
+        <Slider {...settings}>
+          {globalStory.storyPaged.slice(1).map((page, index) =>
+            <div key={index}>
+              <Text className='lead'>
+                {page}
+              </Text>
+            </div>)}
+        </Slider>
 
-          <GridItem colSpan={{ lg: 8, md: 10, base: 12 }}>
-            <Heading as='h1' className='heading-small' mb={3} mt={10}>{title}</Heading>
-            <Text className='lead'>
-              {globalStory.storyPaged[globalStory.currentPage]}
-            </Text>
-            <Steps currentStep={globalStory.currentPage} size={pages} />
-          </GridItem>
-
-          <GridItem colSpan={{ lg: 2, md: 1, base: 0 }} className={styles.buttonWrapper}>
-            <Button className='big secondary only-icon' rightIcon={<Image src='/icons/Arrow-Right.svg' alt='Arrow right outline white icon' />} disabled={globalStory.currentPage >= pages - 1} onClick={handleNextPage} />
-          </GridItem>
-
-        </Grid>
       </Container>
 
       {/* Modal to display */}
