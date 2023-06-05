f1867c3e7bdb98f9a2fb4978de56d4f181e60770
florlapetina <florencia@moka.tv>
2023-06-05 12:36:03 -0300

    modal wip

--- src/app/components/ModalWrapper/ModalWrapper.tsx
+++ src/app/components/ModalWrapper/ModalWrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Container, Image, Grid, GridItem } from '@chakra-ui/react'
+import { Modal, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, Container, Image, Grid, Text, GridItem, Slide, Box, useMediaQuery } from '@chakra-ui/react'
 import React, { ReactElement, ReactNode } from 'react'
 
 interface Props {
@@ -18,54 +18,82 @@ interface Props {
 }
 
 function ModalWrapper ({ isOpen, onClose, modalTitle, children, primaryActionLabel, primaryAction, secondaryActionLabel, secondaryAction, rightIconPrimaryAction, alignmentBottom, rightIconSecondaryAction }: Props) {
+  const [isMobile] = useMediaQuery('(max-width: 600px)')
+  const modalContent = (
+    <Container>
+      <Grid templateColumns='repeat(12, 1fr)' gap={4}>
+        <GridItem colSpan={{ lg: 2, md: 0, base: 0 }} />
+        <GridItem colSpan={{ lg: 8, md: 12, base: 12 }}>
+          <div className='heading-small'>{modalTitle}</div>
+          <div className='body'>
+            {children}
+          </div>
+          <div>
+            {primaryActionLabel &&
+              <Button
+                mr={3}
+                onClick={primaryAction}
+                className='small primary'
+                rightIcon={rightIconPrimaryAction}
+              >
+                {primaryActionLabel}
+              </Button>}
+            {secondaryActionLabel &&
+              <Button
+                className={`${alignmentBottom ? 'md_secondary_border' : 'small secondary'}`}
+                onClick={secondaryAction}
+                rightIcon={rightIconSecondaryAction}
+              >
+                <label>{secondaryActionLabel}</label>
+              </Button>}
+            {alignmentBottom &&
+              <div className='back-bottom-button'>
+                <Button
+                  variant='ghost'
+                  className='small'
+                  onClick={onClose}
+                  leftIcon={<Image src='/icons/Arrow-Left.svg' alt='Back arrow outline icon' />}
+                >
+                  <label>Back to Story</label>
+                </Button>
+              </div>}
+          </div>
+        </GridItem>
+        <GridItem colSpan={{ lg: 2, md: 0, base: 0 }} />
+      </Grid>
+    </Container>
+  )
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose} motionPreset='slideInBottom'>
-      <ModalContent className={alignmentBottom ? 'modal-bottom' : ''}>
-        <ModalCloseButton />
-        <Container>
-          <Grid templateColumns='repeat(12, 1fr)' gap={4}>
-            <GridItem colSpan={{ lg: 2, md: 0, base: 0 }} />
-            <GridItem colSpan={{ lg: 8, md: 12, base: 12 }}>
-              <ModalHeader className='heading-small'>{modalTitle}</ModalHeader>
-              <ModalBody className='body'>
-                {children}
-              </ModalBody>
-              <ModalFooter>
-                {primaryActionLabel &&
-                  <Button
-                    mr={3}
-                    onClick={primaryAction}
-                    className='small primary'
-                    rightIcon={rightIconPrimaryAction}
-                  >
-                    {primaryActionLabel}
-                  </Button>}
-                {secondaryActionLabel &&
-                  <Button
-                    className={`${alignmentBottom ? 'md_secondary_border' : 'small secondary'}`}
-                    onClick={secondaryAction}
-                    rightIcon={rightIconSecondaryAction}
-                  >
-                    <label>{secondaryActionLabel}</label>
-                  </Button>}
-                {alignmentBottom &&
-                  <div className='back-bottom-button'>
-                    <Button
-                      variant='ghost'
-                      className='small'
-                      onClick={onClose}
-                      leftIcon={<Image src='/icons/Arrow-Left.svg' alt='Back arrow outline icon' />}
-                    >
-                      <label>Back to Story</label>
-                    </Button>
-                  </div>}
-              </ModalFooter>
-            </GridItem>
-            <GridItem colSpan={{ lg: 2, md: 0, base: 0 }} />
-          </Grid>
-        </Container>
-      </ModalContent>
-    </Modal>
+    <>
+      {(isMobile && !alignmentBottom)
+        ? <>
+          <Slide
+            direction='bottom' in={isOpen} style={{ zIndex: 10 }}
+          >
+            {/* <div className='backdrop' onClick={onClose} /> */}
+            <Box
+              p='40px'
+              color='white'
+              mt='4'
+              bg='teal.500'
+              rounded='md'
+              shadow='md'
+            >
+              <Text onClick={onClose}>close</Text>
+              {modalContent}
+            </Box>
+          </Slide>
+        </>
+        : <Modal
+            isOpen={isOpen} onClose={onClose} motionPreset='slideInBottom' size='full'
+          >
+          <ModalContent className={alignmentBottom ? 'modal-bottom' : ''}>
+            <ModalCloseButton />
+            {modalContent}
+          </ModalContent>
+          </Modal>}
+    </>
   )
 }
 
