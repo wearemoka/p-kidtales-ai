533d4af985118cdd60f29fa34c431de0eabb689c
Ivan Schweikofski <schweikofski@gmail.com>
2023-07-20 10:05:37 -0300

    Fix first render theme

--- src/app/providers/theme.ts
+++ src/app/providers/theme.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import { css, extendTheme, type ThemeConfig } from '@chakra-ui/react'
-import type { StyleFunctionProps } from '@chakra-ui/styled-system'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { StyleFunctionProps } from '@chakra-ui/styled-system'
 
 // configurations of styles
 export enum ColorsTheme {
@@ -20,10 +20,24 @@ const styles = {
     global: (props: StyleFunctionProps) => ({
       body: {
         fontFamily: 'Plus Jakarta Sans',
-        bg: 'radial-gradient(50% 50% at 50% 50%, rgba(151, 69, 251, 0.2) 0%, rgba(151, 69, 251, 0) 100%), #0C0416;'
+        bg: 'radial-gradient(50% 50% at 50% 50%, rgba(151, 69, 251, 0.2) 0%, rgba(151, 69, 251, 0) 100%), #0C0416;',
+        backgroundRepeat: 'no-repeat',
+        color: 'white'
+      },
+      ':root': {
+        'font-family': 'Plus Jakarta Sans !important',
+        '--chakra-fonts-heading': 'Plus Jakarta Sans',
+        '--chakra-fonts-body': 'Plus Jakarta Sans'
+      },
+      '.chakra-container': {
+        'max-width': '100%',
+        '@media screen and (min-width: 767px)': {
+          'max-width': '90vw'
+        },
+        '@media screen and (min-width: 991px)': {
+          'max-width': '80vw'
+        }
       }
-      // '@import': '../styles/globals.scss'
-      // sx: '../styles/globals.scss'
     })
   }
 }
