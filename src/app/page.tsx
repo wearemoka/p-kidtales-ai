201b7ba177c5f34b420e001bec1d30f53ec3ec76
Ivan Schweikofski <schweikofski@gmail.com>
2023-07-20 11:48:51 -0300

    Move config firebase near to the services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,6 +17,7 @@ const HomePage = () => {
 
   useEffect(() => {
     setGlobalPrompt(emptyPrompt)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const startCreateStoryButtonHandler = () => {
