432adebda8d3b053f9aca36faee4c13ac5d32fd6
Ivan Schweikofski <schweikofski@gmail.com>
2023-07-21 08:00:40 -0300

    Remove old functions - unused

--- src/app/services/ChatGPTService.ts
+++ src/app/services/ChatGPTService.ts
@@ -1,31 +1,3 @@
-import { getStoryIllustrationPrompt, headerOpenAiRequest } from '@/app/utils/promptsGenerators'
-
-// Some declarations
-const URI_API = process.env.NEXT_PUBLIC_OPENAI_API_URL
-
-/**
- * Requests a img from the AI using the about parameter
- * @param about string, what the illustration is about
- * @returns promise
- */
-export async function getAiIllustration (about: string) {
-  try {
-    const prompt = getStoryIllustrationPrompt(about)
-
-    const res = await fetch(`${URI_API}/images/generations`, {
-      method: 'POST',
-      body: prompt,
-      headers: headerOpenAiRequest,
-      cache: 'no-store'
-    })
-
-    const data = res.json()
-    return data
-  } catch (err) {
-    console.error('catch', err)
-  }
-}
-
 /**
  * Requests a story to the AI using internal API
  * @param ageRange the age range, e.g. 5-7
