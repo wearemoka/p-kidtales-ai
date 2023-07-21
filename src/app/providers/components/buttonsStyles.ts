5ac3d0af3b1ccbba42698756eedfb2dbecf31efe
Ivan Schweikofski <schweikofski@gmail.com>
2023-07-21 12:08:13 -0300

    Added default buttons and Font family on theme

---
+++ src/app/providers/components/buttonsStyles.ts
@@ -0,0 +1,9 @@
+import { defineStyleConfig } from '@chakra-ui/react'
+
+export const ButtonStyles = defineStyleConfig({
+  // The styles all button have in common
+  baseStyle: {
+    borderRadius: '100px',
+    height: 'auto'
+  }
+})
--- src/app/providers/theme.ts
+++ src/app/providers/theme.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 import { StyleFunctionProps } from '@chakra-ui/styled-system'
-
+import { ButtonStyles as Button } from './components/buttonsStyles'
 // configurations of styles
 export enum ColorsTheme {
   LIGHT = 'light',
@@ -14,20 +14,26 @@ const config: ThemeConfig = {
   useSystemColorMode: false
 }
 
+const myCustomComponents = {
+  components: {
+    Button
+  }
+}
+
 // global colors
 const styles = {
   styles: {
     global: (props: StyleFunctionProps) => ({
       body: {
-        fontFamily: 'Plus Jakarta Sans',
+        fontFamily: 'Plus Jakarta Sans, sans-serif',
         bg: 'radial-gradient(50% 50% at 50% 50%, rgba(151, 69, 251, 0.2) 0%, rgba(151, 69, 251, 0) 100%), #0C0416;',
         backgroundRepeat: 'no-repeat',
         color: 'white'
       },
       ':root': {
-        fontFamily: 'Plus Jakarta Sans !important',
-        '--chakra-fonts-heading': 'Plus Jakarta Sans',
-        '--chakra-fonts-body': 'Plus Jakarta Sans'
+        fontFamily: 'Plus Jakarta Sans, sans-serif !important',
+        '--chakra-fonts-heading': 'Plus Jakarta Sans, sans-serif',
+        '--chakra-fonts-body': 'Plus Jakarta Sans, sans-serif'
       },
       '.chakra-container': {
         maxWidth: '100%',
@@ -43,6 +49,6 @@ const styles = {
 }
 
 // Extend the theme
-const theme = extendTheme({ config, ...styles })
+const theme = extendTheme({ config, ...styles, ...myCustomComponents })
 
 export default theme
