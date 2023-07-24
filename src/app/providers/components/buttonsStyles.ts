e143c9f7b966cc868f9d00676aa162ef72f106da
Ivan Schweikofski <schweikofski@gmail.com>
2023-07-24 09:32:24 -0300

    Added basics definitions on themes for Chakra UI

--- src/app/providers/components/buttonsStyles.ts
+++
@@ -1,9 +0,0 @@
-import { defineStyleConfig } from '@chakra-ui/react'
-
-export const ButtonStyles = defineStyleConfig({
-  // The styles all button have in common
-  baseStyle: {
-    borderRadius: '100px',
-    height: 'auto'
-  }
-})
--- src/app/providers/theme.ts
+++ src/app/providers/theme.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 import { StyleFunctionProps } from '@chakra-ui/styled-system'
-import { ButtonStyles as Button } from './components/buttonsStyles'
+
 // configurations of styles
 export enum ColorsTheme {
   LIGHT = 'light',
@@ -16,7 +16,52 @@ const config: ThemeConfig = {
 
 const myCustomComponents = {
   components: {
-    Button
+    Container: {
+      baseStyle: {
+        maxWidth: '100%',
+        '@media screen and (min-width: 767px)': {
+          maxWidth: '90vw'
+        },
+        '@media screen and (min-width: 991px)': {
+          maxWidth: '80vw'
+        }
+      }
+    },
+    Grid: {
+      baseStyle: {
+        templateColumns: 'repeat(12, 1fr)',
+        gap: 4
+      }
+    },
+    GridItem: {
+      baseStyle: {
+        colSpan: { lg: 2, md: 0, sm: 0, base: 0 }
+      }
+    },
+    Heading: {
+      baseStyle: {
+        fontSize: '46px !important',
+        fontWeight: 'bold',
+        fontFamily: "'Plus Jakarta Sans', sans-serif",
+        lineHeight: '130%'
+      },
+      sizes: {
+        '2xl': { fontSize: '2xl', lineHeight: '3xl' },
+        xl: { fontSize: 'xl', lineHeight: '2xl' },
+        lg: { fontSize: 'lg', lineHeight: 'xl' },
+        md: { fontSize: 'md', lineHeight: 'base' },
+        sm: { fontSize: 'sm', lineHeight: 'base' }
+      }
+    },
+    Button: {
+      baseStyle: {
+        fontFamily: "'Plus Jakarta Sans'",
+        fontWeight: 'bold',
+        borderRadius: '100px',
+        _focus: { boxShadow: 'outline' },
+        color: 'white'
+      }
+    }
   }
 }
 
