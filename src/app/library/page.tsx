035acbcb44400ec6a705a7eddb888c0f0c53a28d
Ivan Schweikofski <schweikofski@gmail.com>
2023-07-25 11:32:02 -0300

    Added missing container

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,18 +1,22 @@
+'use client'
+import { Container, Heading } from '@chakra-ui/react'
 import Stories from '../components/Library/Library'
 import { ages } from '@/app/services/constants/StoryParams'
 
 function LibraryPage () {
   return (
     <>
-      <div>
-        <h1 className='heading-small'>Popular Stories</h1>
+      <Container>
+        <Heading as='h1' className='heading-small'>
+          Popular Stories
+        </Heading>
 
         {ages.map((age:string, item: number) => {
           return (
             <Stories age={age} key={item} />
           )
         })}
-      </div>
+      </Container>
     </>
   )
 }
